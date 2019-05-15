049d8a910232f073a21fc495a782fad4138a4db0
maciejkola <matthias.kola@gmail.com>
2019-05-15 14:36:05 +0200

    Update compare_file_date.py

--- checks/compare_file_date.py
+++ checks/compare_file_date.py
@@ -5,13 +5,14 @@ import argparse
 import sys
 import os
 from datetime import datetime, timedelta
+import glob
 
 # exit 0 - OK
 # exit 1 - WARNING
 # exit 2 - CRITICAL
 # exit 3 - UNKNOWN
 
-def check(PATH, DAYS):
+def check_file(PATH, DAYS):
     # sanitaze user input:
     #PATH = arguments[0]
     #DAYS = arguments[1]
@@ -48,18 +49,44 @@ def check(PATH, DAYS):
         sys.exit(3)
 
 
+def check_dir(DIR, DAYS):
+    # some sanity checks:
+
+    # check is second argument is insteger
+
+    # check if file exists
+    if not os.path.isdir(DIR):
+        print("CRITICAL - Path to directory does not exist: %s" % DIR)
+        sys.exit(2)
+
+    # check if directory is empty:
+    if not os.listdir(DIR):
+        print("CRITICAL - Directory %s is empty - backup broken!" % DIR)
+        sys.exit(2)
+
+    # get the latest file in directory
+    LIST_OF_FILES = glob.glob(DIR + '/*')
+    LATEST_FILE = max(LIST_OF_FILES, key=os.path.getctime)
+
+    # check if this file is older than defined value:
+    check_file(LATEST_FILE, DAYS)
 
 
 parser=argparse.ArgumentParser(
             description='''Check if date of file creation is older than given number of days ''',
                 epilog="""Designed by mkola for Icinga 2 monitoring.""")
-parser.add_argument('--path', type=str, help='Path to file')
+parser.add_argument('--file', type=str, help='Path to file')
 parser.add_argument('--days', type=str, help='Age of file: not older than this value')
+parser.add_argument('--dir', type=str, help='Path to directory in which we should search for newest file')
 args=parser.parse_args()
 
 if not len(sys.argv) > 1:
     parser.print_help(sys.stderr)
     sys.exit(3)
 
-if args.path and args.days:
-    check(args.path, args.days)
+if args.file and args.days:
+    check_file(args.file, args.days)
+
+if args.dir and args.days:
+    check_dir(args.dir, args.days)
+
